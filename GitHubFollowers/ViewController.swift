3aceff03e4bc4683372d6d4b3ad7d014ad473663
Elizeu RS <13ders@gmail.com>
2024-04-15 16:59:17 -0300

    xcode project setup - delete storyboard

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Elizeu RS on 15/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemPink
+  }
+
+
+}
+
