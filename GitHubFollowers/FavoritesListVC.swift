14acb7b064c3bd34d3fba0398c5257bee67ff670
Elizeu RS <13ders@gmail.com>
2024-04-16 15:40:52 -0300

    App Navigation Setup - TabBar & NavBar

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Elizeu RS on 16/04/24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Elizeu RS on 16/04/24.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      view.backgroundColor = .systemPink
+    }
+
+}
