bfdefe619d1bf9aa472791fdd7a36fc17fe42481
Elizeu RS <13ders@gmail.com>
2024-05-18 20:21:05 -0300

    Child ViewController - UserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Elizeu RS on 18/05/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+  static let location =  "mappin.and.ellipse"
+}
