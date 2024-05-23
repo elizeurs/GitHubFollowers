4dcd068a0c92d663021a579cde0be6a7729ccfd9
Elizeu RS <13ders@gmail.com>
2024-05-23 19:13:39 -0300

    Date Conversion & Date Label

--- GitHubFollowers/Custom Views/Screens/UserInfoVC.swift
+++ GitHubFollowers/Custom Views/Screens/UserInfoVC.swift
@@ -12,6 +12,7 @@ class UserInfoVC: UIViewController {
   let headerView          = UIView()
   let itemViewOne         = UIView()
   let itemViewTwo         = UIView()
+  let dateLabel           = GFBodyLabel(textAlignment: .center)
   var itemViews: [UIView] = []
   
   var username: String!
@@ -42,6 +43,9 @@ class UserInfoVC: UIViewController {
           self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
           self.add(childVC: GFRepoItemVC(user: user), to: self.itemViewOne)
           self.add(childVC: GFFollowerItemVC(user: user), to: self.itemViewTwo)
+//          self.dateLabel.text = "Oct 13"
+//          self.dateLabel.text = user.createdAt
+          self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
         }
 //        print(user)
         
@@ -55,7 +59,7 @@ class UserInfoVC: UIViewController {
     let padding: CGFloat    = 20
     let itemHeight: CGFloat = 140
     
-    itemViews = [headerView, itemViewOne, itemViewTwo]
+    itemViews = [headerView, itemViewOne, itemViewTwo, dateLabel]
     
     for itemView in itemViews {
       view.addSubview(itemView)
@@ -79,6 +83,9 @@ class UserInfoVC: UIViewController {
       
       itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
       itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+      
+      dateLabel.topAnchor.constraint(equalTo: itemViewTwo.bottomAnchor, constant: padding),
+      dateLabel.heightAnchor.constraint(equalToConstant: 18)
     ])
   }
   
