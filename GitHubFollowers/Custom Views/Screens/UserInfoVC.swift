d4e12b96bc0a9613de9094fca387aa383814633b
Elizeu RS <13ders@gmail.com>
2024-06-17 14:02:43 -0300

    Optimization - Old iPhone SE - UserInfoVC Fixes

--- GitHubFollowers/Custom Views/Screens/UserInfoVC.swift
+++ GitHubFollowers/Custom Views/Screens/UserInfoVC.swift
@@ -13,6 +13,9 @@ protocol UserInfoVCDelegate: AnyObject {
 
 class UserInfoVC: GFDataLoadingVC {
   
+  let scrollView          = UIScrollView()
+  let contentView         = UIView()
+  
   let headerView          = UIView()
   let itemViewOne         = UIView()
   let itemViewTwo         = UIView()
@@ -24,18 +27,33 @@ class UserInfoVC: GFDataLoadingVC {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-   configureViewController()
+    configureViewController()
+    configureScrollView()
     layoutUI()
     getUserInfo()
     
-//    print(username!)
-  
- }
+    //    print(username!)
+    
+  }
   
   func configureViewController() {
     view.backgroundColor = .systemBackground
     let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
     navigationItem.rightBarButtonItem = doneButton
+  }
+  
+  func configureScrollView() {
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentView)
+//    scrollView.translatesAutoresizingMaskIntoConstraints = false
+//    contentView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.pinToEdges(of: contentView)
+    scrollView.pinToEdges(of: scrollView)
+    
+    NSLayoutConstraint.activate([
+      contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+      contentView.heightAnchor.constraint(equalToConstant: 2000)
+    ])
   }
   
   func getUserInfo() {
@@ -45,9 +63,9 @@ class UserInfoVC: GFDataLoadingVC {
       switch result {
       case .success(let user):
         DispatchQueue.main.async { self.configureUIElements(with: user)
-        //        print(user)
-        self.configureUIElements(with: user)
-      }
+          //        print(user)
+          self.configureUIElements(with: user)
+        }
         
       case .failure(let error):
         self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
@@ -63,7 +81,7 @@ class UserInfoVC: GFDataLoadingVC {
     //          self.dateLabel.text = user.createdAt
     // self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
     self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
-
+    
   }
   
   func layoutUI() {
@@ -73,29 +91,29 @@ class UserInfoVC: GFDataLoadingVC {
     itemViews = [headerView, itemViewOne, itemViewTwo, dateLabel]
     
     for itemView in itemViews {
-      view.addSubview(itemView)
+      contentView.addSubview(itemView)
       itemView.translatesAutoresizingMaskIntoConstraints = false
       
       NSLayoutConstraint.activate([
-        itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
-        itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+        itemView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+        itemView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding)
       ])
     }
     
-//    itemViewOne.backgroundColor = .systemPink
-//    itemViewTwo.backgroundColor = .systemBlue
+    //    itemViewOne.backgroundColor = .systemPink
+    //    itemViewTwo.backgroundColor = .systemBlue
     
     NSLayoutConstraint.activate([
       headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
       headerView.heightAnchor.constraint(equalToConstant: 210),
       
-      itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+      itemViewOne.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: padding),
       itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
       
-      itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+      itemViewTwo.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: padding),
       itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight),
       
-      dateLabel.topAnchor.constraint(equalTo: itemViewTwo.bottomAnchor, constant: padding),
+      dateLabel.topAnchor.constraint(equalTo: contentView.bottomAnchor, constant: padding),
       dateLabel.heightAnchor.constraint(equalToConstant: 50)
     ])
   }
@@ -114,29 +132,29 @@ class UserInfoVC: GFDataLoadingVC {
 
 extension UserInfoVC: GFRepoItemVCDelegate {
   
-    func didTapGitHubProfile(for user: User) {
-      // Show safari view controller
-  //    print("My button was tapped!!!")
-      guard let url = URL(string: user.htmlUrl) else {
-        presentGFAlertOnMainThread(title: "Invalid URL", message: "The url attached to this user is invalid.", buttonTitle: "Ok")
-        return
-      }
-  
-      presentSafariVC(with: url)
+  func didTapGitHubProfile(for user: User) {
+    // Show safari view controller
+    //    print("My button was tapped!!!")
+    guard let url = URL(string: user.htmlUrl) else {
+      presentGFAlertOnMainThread(title: "Invalid URL", message: "The url attached to this user is invalid.", buttonTitle: "Ok")
+      return
     }
+    
+    presentSafariVC(with: url)
+  }
 }
 
 extension UserInfoVC: GFFollowerItemVCDelegate {
   
-    func didTapGetFollowers(for user: User) {
-      // dismissvc
-      // tell follower list screen the new user
-      guard user.followers != 0 else {
-        presentGFAlertOnMainThread(title: "No followers", message: "This user has no followers. What a shame 😞.", buttonTitle: "So sad")
-        return
-      }
-  
-      delegate.didRequestFollowers(for: user.login)
-      dismissVC()
+  func didTapGetFollowers(for user: User) {
+    // dismissvc
+    // tell follower list screen the new user
+    guard user.followers != 0 else {
+      presentGFAlertOnMainThread(title: "No followers", message: "This user has no followers. What a shame 😞.", buttonTitle: "So sad")
+      return
     }
+    
+    delegate.didRequestFollowers(for: user.login)
+    dismissVC()
+  }
 }
--- GitHubFollowers/Extensions/UIView+Ext.swift
+++ GitHubFollowers/Extensions/UIView+Ext.swift
@@ -9,8 +9,18 @@ import UIKit
 
 extension UIView {
   
+  func pinToEdges(of superview: UIView) {
+    translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      topAnchor.constraint(equalTo: superview.topAnchor),
+      leadingAnchor.constraint(equalTo: superview.leadingAnchor),
+      trailingAnchor.constraint(equalTo: superview.trailingAnchor),
+      bottomAnchor.constraint(equalTo: superview.bottomAnchor)
+    ])
+  }
+  
   // Variadic Parameter - i can pass any number of views into addSubviews. turns views into a array.
   func addSubviews(_ views: UIView...) {
-      for view in views { addSubview(view) }
-    }
+    for view in views { addSubview(view) }
   }
+}
