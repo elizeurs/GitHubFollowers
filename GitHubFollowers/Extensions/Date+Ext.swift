4338f5c793240f7e2a68243d674312672aedd360
Elizeu RS <13ders@gmail.com>
2024-06-26 13:42:26 -0300

    iOS 15 - Date Format

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -9,9 +9,20 @@ import Foundation
 
 extension Date {
   
+  // new way
   func convertToMonthYearFormat() -> String {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "MMM yyyy"
-    return dateFormatter.string(from: self)
+    return formatted(.dateTime.month().year())
+    // options available.
+    // option + click .month, you can see optitons available and/or click on " Open in Developer Documentation" after that.
+    //    return formatted(.dateTime.month(.wide).year(.twoDigits))
+    // default
+//    return formatted()
   }
+  
+  // old way
+//  func convertToMonthYearFormat() -> String {
+//    let dateFormatter = DateFormatter()
+//    dateFormatter.dateFormat = "MMM yyyy"
+//    return dateFormatter.string(from: self)
+//  }
 }
